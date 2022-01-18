b473ebb32501944d44a036fccbd353091c32d9cc
Ramon <rsanchezabadia@cifpfbmoll.eu>
2022-01-18 19:28:58 +0100

    fix: :bug: fix some bugs and still working in it

--- spinsCards.py
+++ spinsCards.py
@@ -3,54 +3,55 @@ import string
 class spinsCards:
     def __init__(self, pins):
         self.pins = pins
-        self.totalPuntuation = self.calculatePins(pins)
 
-    def setTotal(self, totalPuntuation):
-        self.totalPuntuation = totalPuntuation
+    # Sustituye los - por 0
+    def replaceDash(self):
+        self.pins = self.pins.replace("-", "0")
 
-    def getTotal(self):
-        return self.totalPuntuation
-
-    def calculatePins(self, pins):
+    def calculatePins(self):
+        self.replaceDash()
+        pins = self.pins
         isSpare, isStrike = False, False
-
+        i = 0
+        totalPuntuation = 0
         for bowlingToss in range(len(pins)):
-            tossStrike = 0
             framePuntuation = 0
-
-            # Si es strike, spare, 0 o número
+            
+            # Si es strike, spare o número
             if pins[bowlingToss] not in string.digits:
                 match pins[bowlingToss]:
                     case "/":
                         framePuntuation = 10 - int(pins[bowlingToss-1])
                         isSpare = True
-                        break
                     case "X":
                         framePuntuation = 10
-                        tossStrike += 2
                         isStrike = True
-                        break
-                    case "-":
-                        framePuntuation = 0
-                        break
             else:
                 framePuntuation = int(pins[bowlingToss])
             
             # Tirada siguiente  a "/"
-            if isSpare:
+            if isSpare and pins[bowlingToss] != "/":
                 framePuntuation *= 2
                 isSpare = False
 
             # Tirada siguiente a "X"
-            if tossStrike != 0:
-                if isStrike:
-                    framePuntuation *=3
-                    isStrike = False
-                else:
-                    framePuntuation *=2
-                    
-                tossStrike -= 1
-
+            if i != 0:
+                framePuntuation *= 2
+                i -= 1
+                
             totalPuntuation += framePuntuation
 
+            if isStrike:
+                i += 2
+                isStrike = False
+
+        print(totalPuntuation)
         return totalPuntuation
+
+if __name__ == '__main__':
+    def prueba():
+        pins = "XXX"
+        total = 60
+        SpinsCards = spinsCards(pins)
+        assert SpinsCards.calculatePins() == total
+    prueba()
